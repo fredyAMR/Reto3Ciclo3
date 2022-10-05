c0e157d39e302fdf36b9fbd72c9c2b11db5e609f
famrARM <fredy.moyano.mt@usa.edu.co>
2022-10-05 16:04:35 -0500

    reto3

---
+++ src/main/java/com/example/demo/Ret3G40Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ret3G40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ret3G40Application.class, args);
+	}
+
+}
